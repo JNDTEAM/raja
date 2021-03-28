8e62bbfbf78f2085fa8db54e8b3412afb9fea617
JNDTEAM <62438778+JNDOFFICIAL@users.noreply.github.com>
2021-03-28 20:00:18 +0700

    Update multi.sh

--- multi.sh
+++ multi.sh
@@ -1,46 +1,60 @@
 php enc.php &
 sleep 60
-cd raja/1
+cd 1
 php enc.php &
 sleep 60
-cd raja/2
+cd ..
+cd 2
 php enc.php &
 sleep 60
-cd raja/3
+cd ..
+cd 3
 php enc.php &
 sleep 60
-cd raja/4
+cd ..
+cd 4
 php enc.php &
 sleep 60
-cd raja/5
+cd ..
+cd 5
 php enc.php &
 sleep 60
-cd raja/6
+cd ..
+cd 6
 php enc.php &
 sleep 60
-cd raja/7
+cd ..
+cd 7
 php enc.php &
 sleep 60
-cd raja/8
+cd ..
+cd 8
 php enc.php &
 sleep 60
-cd raja/9
-php enc.php & 
-cd raja/10
+cd ..
+cd 9
 php enc.php &
 sleep 60
-cd raja/11
+cd ..
+cd 10
 php enc.php &
 sleep 60
-cd raja/12
+cd ..
+cd 11
 php enc.php &
 sleep 60
-cd raja/13
+cd ..
+cd 12
 php enc.php &
 sleep 60
-cd raja/14
+cd ..
+cd 13
 php enc.php &
 sleep 60
-cd raja/15
+cd ..
+cd 14
 php enc.php &
 sleep 60
+cd ..
+cd 15
+php enc.php &
