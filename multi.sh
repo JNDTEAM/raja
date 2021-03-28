121f681e1cfd30506c5081753bf5f207dfe29b01
JNDTEAM <62438778+JNDOFFICIAL@users.noreply.github.com>
2021-03-28 19:37:28 +0700

    Add files via upload

---
+++ multi.sh
@@ -0,0 +1,46 @@
+php enc.php &
+sleep 60
+cd raja/1
+php enc.php &
+sleep 60
+cd raja/2
+php enc.php &
+sleep 60
+cd raja/3
+php enc.php &
+sleep 60
+cd raja/4
+php enc.php &
+sleep 60
+cd raja/5
+php enc.php &
+sleep 60
+cd raja/6
+php enc.php &
+sleep 60
+cd raja/7
+php enc.php &
+sleep 60
+cd raja/8
+php enc.php &
+sleep 60
+cd raja/9
+php enc.php & 
+cd raja/10
+php enc.php &
+sleep 60
+cd raja/11
+php enc.php &
+sleep 60
+cd raja/12
+php enc.php &
+sleep 60
+cd raja/13
+php enc.php &
+sleep 60
+cd raja/14
+php enc.php &
+sleep 60
+cd raja/15
+php enc.php &
+sleep 60
